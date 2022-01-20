20471673ddbe738c3d7ad6de207c7b0096b1afab
Tobias Tangemann <tobias@tangemann.org>
2022-01-20 23:52:11 +0100

    Add startup delay + exponential backoff

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use get_if_addrs::{get_if_addrs, IfAddr, Interface};
 use log::{debug, error, info};
 
+use std::cmp;
 use std::process::Command;
 use std::str::FromStr;
 use std::thread;
@@ -24,6 +25,10 @@ struct Config {
     /// Intervall (in seconds) to check the interface
     #[clap(long, default_value="info", parse(try_from_str = log::Level::from_str), env)]
     log_level: log::Level,
+
+    /// Time to wait before first check (in seconds)
+    #[clap(long, default_value = "300", env)]
+    startup_delay: u16,
 }
 
 fn main() {
@@ -33,7 +38,16 @@ fn main() {
     // Initialize logger
     simple_logger::init_with_level(cfg.log_level).unwrap();
 
+    if cfg.startup_delay > 0 {
+        info!("Waiting startup delay of {} seconds", cfg.startup_delay);
+        thread::sleep(Duration::from_secs(cfg.startup_delay.into()));
+        info!("Starting check loop");
+    }
+
+    let mut exponential_backoff = 0u8;
     loop {
+        thread::sleep(Duration::from_secs(u64::pow(2, exponential_backoff as u32)));
+
         let global_v6_addresses = get_global_v6_addresses(&cfg.interface);
 
         if let Err(err) = global_v6_addresses {
@@ -55,9 +69,13 @@ fn main() {
                     }
                 );
 
+                // wait up to 2**9 = 512s (8m32s) in case of continous reconfigures
+                exponential_backoff = cmp::min(exponential_backoff + 1, 9);
+
                 thread::sleep(Duration::from_secs(5));
                 continue;
             } else {
+                exponential_backoff = 0;
                 debug!("Global ipv6 addresses: {addresses:?}");
             }
         }
